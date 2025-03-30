ab34d793f4f84227e432eba06c6240e4488a2fe7
sourabh7204 <sourabh722004@gmail.com>
2025-03-30 10:37:41 +0530

    Inserted GET method

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 import { faker } from '@faker-js/faker';
 import mysql from 'mysql2/promise';
-
-  // Connect to MySQL
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'sigma_app',
-    password: 'sourabh7204',
-  });
-
-  
-
-  try {
-    const [result] = await connection.query("SHOW TABLES");
-    console.log(result);
-  } catch (err) {
-    console.error(err);
-  }
-
-  connection.end();
-
-  // Generate Random User
-  const getRandomUser = () => {
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  };
+import express from "express";
+const app=express();
 
 
-//   console.log(getRandomUser());
+
+// Create a connection
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'sigma_app',
+  password: 'sourabh7204',
+});
+
+// Generate a Random User
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),            // Generates a unique ID
+    faker.internet.username(),      
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
 
 
+
+app.get("/", (req,res)=>{
+  res.send("Welcome to home page");
+});
+
+app.listen("8080" , () =>{
+  console.log("Server is Listening to 8080 Port...")
+});
+
+// // SQL query for multiple inserts
+// let q = "INSERT INTO temp (id, username, email, password) VALUES ?";
+
+
+// try {
+//   // Use [data] to insert multiple rows correctly
+//   const [result] = await connection.query(q, [data]);
+//   console.log("Rows inserted successfully:", result);
+// } catch (err) {
+//   console.error("Error inserting rows:", err);
+// }
+
+// // Close the connection
+// await connection.end();
